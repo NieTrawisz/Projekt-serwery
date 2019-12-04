3753a431b1920800cf37fca2260eef0611b49998
NieTrawisz <49477388+NieTrawisz@users.noreply.github.com>
2019-12-04 15:46:51 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,21 +1,25 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import unittest
 from collections import Counter
 
-from servers import ListServer, Product, Client, MapServer
+from servers import ListServer, Product, Client, MapServer,TooManyProductsFoundError
 
 server_types = (ListServer, MapServer)
 
 
 class ServerTest(unittest.TestCase):
-
+    def test_exception(self):
+        with self.assertRaises(TooManyProductsFoundError):
+            max=ListServer.n_max_returned_entries
+            p=Product('a12',1)
+            ls=ListServer([p for i in range(max+1)])
+            ls.get_entries(1)
     def test_get_entries_returns_proper_entries(self):
         products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
         for server_type in server_types:
             server = server_type(products)
             entries = server.get_entries(2)
             self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
+
 
 
 class ClientTest(unittest.TestCase):
@@ -25,7 +29,14 @@ class ClientTest(unittest.TestCase):
             server = server_type(products)
             client = Client(server)
             self.assertEqual(5, client.get_total_price(2))
+    def test_catch_exception(self):
+        max = ListServer.n_max_returned_entries
+        p = Product('a12', 1)
+        ls = ListServer([p for i in range(max + 1)])
+        c=Client(ls)
+        self.assertEqual(None,c.get_total_price(1))
 
 
 if __name__ == '__main__':
     unittest.main()
+
