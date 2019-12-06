e0789215e1b3a22cb57df788d96dfdf3bcef818a
NieTrawisz <49477388+NieTrawisz@users.noreply.github.com>
2019-12-06 17:22:29 +0100

    Update servers.py

--- servers.py
+++ servers.py
@@ -9,8 +9,8 @@ from abc import ABC, abstractmethod
 
 class Product:
     def __init__(self, product_name, product_price):
-        self.product_name = product_name
-        self.product_price = product_price
+        self.name = product_name
+        self.price = product_price
 
     def __hash__(self):
         return hash((self.name, self.price))
@@ -43,14 +43,14 @@ class ListServer(Server):
     def get_entries(self, n) -> List[Product]:
         list = []
         for el in self.product_list:
-            a = re.match('^[a-zA-Z]{{{}}}\\d{{2,3}}$'.format(n), el.product_name)
+            a = re.match('^[a-zA-Z]{{{}}}\\d{{2,3}}$'.format(n), el.name)
             if a != None:
                 list.append(el)
         if len(list) > self.n_max_returned_entries:
             raise TooManyProductsFoundError()
 
 
-        return sorted(list, key = lambda p:p.product_price)
+        return sorted(list, key = lambda p:p.price)
 
 
 class MapServer(Server):
@@ -59,7 +59,7 @@ class MapServer(Server):
     def __init__(self, product_list):
         self.product_dict = {}
         for el in product_list:
-            self.product_dict[el.product_name] = el
+            self.product_dict[el.name] = el
 
     def get_entries(self, n: int) -> List[Product]:
         list = []
@@ -70,7 +70,7 @@ class MapServer(Server):
         if len(list) > self.n_max_returned_entries:
             raise TooManyProductsFoundError()
 
-        return sorted(list, key = lambda p:p.product_price)
+        return sorted(list, key = lambda p:p.price)
 
 
 class Client:
@@ -85,14 +85,5 @@ class Client:
 
         total = 0
         for x in lst:
-            total += x.product_price
+            total += x.price
         return total
-
-
-p = Product('xd126', 4)
-p2 = Product('xg12', 2)
-p3= Product('es164', 6)
-x = MapServer([p,p2,p3])
-c = Client(x)
-
-print(c.get_total_price(2))
